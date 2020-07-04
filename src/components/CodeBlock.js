d4b867f5b3b3034ce5b0b3bfbe453a34a9508f07
Sheharyar Naseer <hello@sheharyar.me>
2020-07-04 05:03:25 +0500

    Add line numbers to code blocks

--- src/components/CodeBlock.js
+++ src/components/CodeBlock.js
@@ -2,8 +2,9 @@ import Prism     from 'prismjs';
 import PropTypes from 'prop-types';
 import React     from 'react';
 
-import "prismjs/components/prism-docker"
-import "prismjs/components/prism-git"
+import 'prismjs/plugins/line-numbers/prism-line-numbers';
+import 'prismjs/components/prism-docker';
+import 'prismjs/components/prism-git';
 
 
 const LANGUAGES = {
@@ -20,7 +21,7 @@ const CodeBlock = ({ code, language }) => {
   })
 
   return (
-    <pre>
+    <pre className="line-numbers">
       <code className={`language-${lang}`}>
         {code}
       </code>
