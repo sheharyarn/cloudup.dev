35a84e70095d22cb9227caf56bf96e21c008ef95
Sheharyar Naseer <hello@sheharyar.me>
2020-07-04 07:05:50 +0500

    Add Google Analytics tracking id

--- gatsby-config.js
+++ gatsby-config.js
@@ -66,7 +66,7 @@ module.exports = {
     {
       resolve: `gatsby-plugin-google-analytics`,
       options: {
-        trackingId: ``,
+        trackingId: `UA-47788797-3`,
       },
     },
     `gatsby-plugin-react-helmet`,
